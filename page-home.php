5536557665eedd409da3bb52eeb8997c68386869
Sam <super8989@gmail.com>
2020-05-28 13:03:20 -0400

    Added Course Features using Custom Post Types and ACF

--- page-home.php
+++ page-home.php
@@ -27,6 +27,10 @@
   $who_section_title      = get_field('who_section_title');
   $who_section_body       = get_field('who_section_body');
 
+  $features_section_image = get_field('features_section_image');
+  $features_section_title = get_field('features_section_title');
+  $features_section_body = get_field('features_section_body');
+
 
   get_header();
 ?>
@@ -166,12 +170,52 @@
     <div class="container">
 
       <div class="section-header">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
-        <h2>Course Features</h2>
+
+        <?php if ( !empty($features_section_image) ) {
+        ?>
+
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
+        
+        <?php
+          }
+        ?>
+        
+        <h2><?php echo $features_section_title; ?></h2>
+
+        <!-- If user added body text -->
+        <?php if ( !empty($features_section_body) ) {
+        ?>
+          <p class="lead"><?php echo $features_section_body; ?></p>
+        
+        <?php 
+          }
+        ?>
+
       </div>
 
       <div class="row">
-        <div class="col-lg-2 col-md-6">
+        
+        <?php $loop = new WP_Query( 
+          array( 
+            'post_type' => 'course_feature', 
+            'orderby'   => 'post_id',
+            'order'     => 'ASC'  
+          )); 
+         
+          while( $loop->have_posts() ) {
+            $loop->the_post();
+        ?>
+
+          <div class="col-lg-2 col-md-6">
+            <i class="<?php the_field('course_feature_icon'); ?>"></i>
+            <h4><?php the_title(); ?></h4>
+          </div>
+
+        <?php
+          } 
+        ?>
+
+        <!-- <div class="col-lg-2 col-md-6">
           <i class="ci ci-computer"></i>
           <h4>Lifetime access to lectures</h4>
         </div>
@@ -194,7 +238,7 @@
         <div class="col-lg-2 col-md-6">
           <i class="ci ci-device"></i>
           <h4>Accessible content on any devices</h4>
-        </div>
+        </div> -->
 
       </div>
 
