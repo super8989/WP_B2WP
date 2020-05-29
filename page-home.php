85655a3361926b1806b7c1266fe0c1ccfb10177e
Sam <super8989@gmail.com>
2020-05-29 14:48:45 -0400

    Testimonial section with custom post

--- page-home.php
+++ page-home.php
@@ -45,6 +45,10 @@
   $num_reviews                  = get_field('num_reviews');
   $num_courses                  = get_field('num_courses');
 
+  $testimonial_title            = get_field('testimonial_title');
+  $testimonial_body             = get_field('testimonial_body');
+  
+
 
   get_header();
 ?>
@@ -401,45 +405,78 @@
       <div class="row">
         <div class="col-sm-8 offset-sm-2">
 
-          <h2>What People Are Saying</h2>
+          <h2><?php echo $testimonial_title; ?></h2>
+          
+          <h4><?php echo $testimonial_body; ?></h4>
+
+          <?php $loop = new WP_Query( 
+            array(
+              'post_type' => 'testimonial',
+              'orderby'   => 'post_id',
+              'order'     => 'ASC'
+            ));
+
+            while ( $loop->have_posts() ) {
+              $loop->the_post();
+          ?>
 
           <div class="row testimonial">
             <div class="col-sm-4">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ernest.png" alt="Ernest">
+              <?php if ( has_post_thumbnail() ) {
+                the_post_thumbnail( array(200, 200) );
+                } 
+              ?>
             </div>
             <div class="col-sm-8 align-self-center">
-              <blockquote>I'm baby vape paleo direct trade, waistcoat succulents banjo normcore cliche heirloom yr
-                narwhal. Adaptogen organic VHS jean shorts, wolf jianbing four loko kinfolk plaid keytar polaroid poke
-                distillery. Tousled quinoa hoodie glossier pork belly pour-over, poutine synth palo santo normcore photo.
-                <cite>&mdash; Ernest, graduate of all courses</cite>
+              <blockquote><?php the_content(); ?>
+                <cite>&mdash; <?php the_title(); ?></cite>
               </blockquote>
             </div>
           </div>
 
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ben.png" alt="Ben">
-            </div>
-            <div class="col-sm-8 align-self-center">
-              <blockquote>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium
-                doloremque obcaecati similique maxime voluptatum animi placeat quidem natus esse culpa!
-                <cite>&mdash; Ben, graduate of all courses</cite>
-              </blockquote>
-            </div>
-          </div>
 
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aj.png" alt="AJ">
-            </div>
-            <div class="col-sm-8 align-self-center">
-              <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi minima
-                molestias pariatur velit voluptatibus, excepturi ab, autem obcaecati fugit, laboriosam vero.
-                <cite>&mdash; AJ, graduate of all courses</cite>
-              </blockquote>
-            </div>
-          </div>
+          <?php
+            }
+          ?>
 
+          <!-- remove
+            <div class="row testimonial">
+              <div class="col-sm-4">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ernest.png" alt="Ernest">
+              </div>
+              <div class="col-sm-8 align-self-center">
+                <blockquote>I'm baby vape paleo direct trade, waistcoat succulents banjo normcore cliche heirloom yr
+                  narwhal. Adaptogen organic VHS jean shorts, wolf jianbing four loko kinfolk plaid keytar polaroid poke
+                  distillery. Tousled quinoa hoodie glossier pork belly pour-over, poutine synth palo santo normcore photo.
+                  <cite>&mdash; Ernest, graduate of all courses</cite>
+                </blockquote>
+              </div>
+            </div>
+
+            <div class="row testimonial">
+              <div class="col-sm-4">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ben.png" alt="Ben">
+              </div>
+              <div class="col-sm-8 align-self-center">
+                <blockquote>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium
+                  doloremque obcaecati similique maxime voluptatum animi placeat quidem natus esse culpa!
+                  <cite>&mdash; Ben, graduate of all courses</cite>
+                </blockquote>
+              </div>
+            </div>
+
+            <div class="row testimonial">
+              <div class="col-sm-4">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aj.png" alt="AJ">
+              </div>
+              <div class="col-sm-8 align-self-center">
+                <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi minima
+                  molestias pariatur velit voluptatibus, excepturi ab, autem obcaecati fugit, laboriosam vero.
+                  <cite>&mdash; AJ, graduate of all courses</cite>
+                </blockquote>
+              </div>
+            </div>
+          remove -->
         </div>
       </div>
     </div>
